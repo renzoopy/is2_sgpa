ac001a4a4d575c38cb42667bf3c83096f4a0da2d
ricklegac <rick.legac@gmail.com>
2021-08-23 14:32:37 -0400

    interfaz al ingresar :8000

--- sgpa/wsgi.py
+++ sgpa/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
-
+ #esta parte estaba en import ... noma por si acaso
 import os
 
 from django.core.wsgi import get_wsgi_application
