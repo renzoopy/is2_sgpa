b1d2c01c769b523bf6a4d9af29ed3a0dca072506
renzoopy <lorenzocabrera8@gmail.com>
2021-09-02 15:44:14 -0400

    Agregados los formularios para el perfil del usuario

---
+++ sgpa/apps/usuarios/forms.py
@@ -0,0 +1,53 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Perfil
+
+
+class Perfil_Form(forms.ModelForm):
+    def save(self, commit=True, *args, **kwargs):
+        ci = kwargs.get("ci")
+        usuario = kwargs.get("usuario")
+        telefono = kwargs.get("telefono")
+        instance = super(Perfil_Form, self).save(commit=False)
+        instance.ci = ci
+        instance.user = usuario
+        instance.telefono = telefono
+        if commit:
+            instance.save()
+
+    class Meta:
+        model = Perfil
+        fields = [
+            "ci",
+            "telefono",
+        ]
+        labels = {
+            "ci": "CI",
+            "telefono": "Teléfono",
+        }
+        widgets = {
+            "ci": forms.TextInput(),
+            "telefono": forms.TextInput(),
+        }
+
+
+class Usuario_Form(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            "first_name",
+            "last_name",
+            "email",
+        ]
+        labels = {
+            "first_name": "Nombre",
+            "last_name": "Apellido",
+            "email": "Email",
+        }
+        widgets = {
+            "first_name": forms.TextInput(attrs={"class": "form-control"}),
+            "last_name": forms.TextInput(attrs={"class": "form-control"}),
+            "email": forms.TextInput(
+                attrs={"class": "form-control", "readonly": "readonly"}
+            ),
+        }
