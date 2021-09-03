962dc8cc37672e1d39243471f2c73fca9ea59b73
renzoopy <lorenzocabrera8@gmail.com>
2021-09-03 01:49:02 -0400

    Urls corregidas

--- sgpa/apps/proyectos/views.py
+++ sgpa/apps/proyectos/views.py
@@ -2,7 +2,7 @@ from usuarios.models import Perfil
 from proyectos.models import Proyecto
 from django.urls.base import reverse_lazy
 from django.views.generic import ListView
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Group, User
 from django.shortcuts import redirect, render
 
 from proyectos.forms import ProyectoAdminForm
@@ -23,19 +23,17 @@ def home(request):
     return render(request, "home.html")
 
 
-def nuevoProyecto(request):
-    return render(request, "proyectos/nuevoProyecto.html")
-
-
 class ProyectoCrear(LoginRequiredMixin, ListView):
     redirect_field_name = "redirect_to"
     model = Proyecto
     form_class = ProyectoAdminForm
-    template_name = "nuevoProyecto.html"
+    template_name = "proyectos/nuevoProyecto.html"
 
     def get_success_url(self):
         scrummaster = Perfil.objects.get(id=self.request.POST["srummaster"])
         proyecto = Proyecto.objects.get(id=self.object.pk)
+        equipo = Group.objects.create(name="comite%s" % self.object.pk)
+        proyecto.equipo = equipo
         proyecto.save()
         return reverse_lazy("proyectos:sprint")
 
