03f668ad833882783c26cafb4a8d57dd9938ede7
renzoopy <lorenzocabrera8@gmail.com>
2021-09-03 15:10:35 -0400

    Urls corregidas

--- sgpa/apps/usuarios/urls.py
+++ sgpa/apps/usuarios/urls.py
@@ -1,11 +1,12 @@
 from os import name
 from django.contrib import admin
 from django.urls import path, include
-from .views import CrearPerfil, editarPerfil, home
+from .views import CrearPerfil, ListarPerfil, editarPerfil, home
 
 
 urlpatterns = [
     path("", home, name="home"),
-    path("perfil_form/", CrearPerfil.as_view(), name="perfil_form"),
+    path("nuevo/", CrearPerfil.as_view(), name="crear_form"),
+    path("listar/", ListarPerfil.as_view(), name="listar_perfiles"),
     path("editar_perfil/<int:id_perfil>/", editarPerfil, name="editar_perfil"),
 ]
