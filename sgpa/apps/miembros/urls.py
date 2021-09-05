d5c509022bb2b58cfe6b8e52f67ff140bd524ce6
renzoopy <lorenzocabrera8@gmail.com>
2021-09-05 04:09:32 -0400

    URL corregida

--- sgpa/apps/miembros/urls.py
+++ sgpa/apps/miembros/urls.py
@@ -3,7 +3,7 @@ from miembros.views import miembroCrear, miembroEliminar, verMiembros
 
 
 urlpatterns = [
-    path("lista/", verMiembros, name="lista"),
+    path("listar/", verMiembros, name="lista"),
     path("nuevo/", miembroCrear, name="nuevo"),
     path("<int:idMiembro>/eliminar/", miembroEliminar, name="eliminar"),
 ]
