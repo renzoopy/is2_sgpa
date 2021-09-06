aa1001410c6e400bfad00b663a81fb208567043b
renzoopy <lorenzocabrera8@gmail.com>
2021-09-05 22:04:12 -0400

    Urls para la edicion y eliminacion de roles agregadas

--- sgpa/apps/roles/urls.py
+++ sgpa/apps/roles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
-from roles.views import CrearRol, ListarRol
+from roles.views import CrearRol, ELiminarRol, EditarRol, ListarRol
 
 urlpatterns = [
     path("nuevo/", CrearRol.as_view(), name="crear_rol"),
     path("listar/", ListarRol.as_view(), name="listar_roles"),
+    path("eliminar/<int:id_rol>/", ELiminarRol, name="eliminar_rol"),
+    path("editar/<int:id_rol>/", EditarRol, name="editar_rol"),
 ]
