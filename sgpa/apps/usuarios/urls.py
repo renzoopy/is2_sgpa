6a656db0665349618c5dc08fd30e38e9b26f1099
renzoopy <lorenzocabrera8@gmail.com>
2021-09-05 22:07:28 -0400

    Agregadas urls para la administracion de usuarios

--- sgpa/apps/usuarios/urls.py
+++ sgpa/apps/usuarios/urls.py
@@ -1,7 +1,17 @@
 from os import name
 from django.contrib import admin
 from django.urls import path, include
-from .views import CrearPerfil, ListarPerfil, editarPerfil, home
+from .views import (
+    CrearPerfil,
+    ListarPerfil,
+    administrador,
+    concederAcceso,
+    editarPerfil,
+    eliminarPerfil,
+    home,
+    listaAcceso,
+    proyectos_usuario,
+)
 
 
 urlpatterns = [
@@ -9,5 +19,9 @@ urlpatterns = [
     path("nuevo/", CrearPerfil.as_view(), name="crear_form"),
     path("listar/", ListarPerfil.as_view(), name="listar_perfiles"),
     path("editar/<int:id_perfil>/", editarPerfil, name="editar_perfil"),
-    #  path("eliminar/<int:id_perfil>/", eliminarPerfil, name="eliminar_perfil"),
+    path("eliminar/<int:id_perfil>/", eliminarPerfil, name="eliminar_perfil"),
+    path("administrador/", administrador, name="administrador"),
+    path("acceso/", listaAcceso, name="lista_acceso"),
+    path("<int:id_usuario>/", proyectos_usuario, name="proyectos_usuario"),
+    path("administrador/<int:id_perfil>", concederAcceso, name="conceder_acceso"),
 ]
