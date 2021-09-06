f9efb5879096a7349a9069565dca25bce3e0712e
renzoopy <lorenzocabrera8@gmail.com>
2021-09-05 22:02:51 -0400

    Correciones en las urls

--- sgpa/apps/proyectos/urls.py
+++ sgpa/apps/proyectos/urls.py
@@ -2,11 +2,11 @@ from os import name
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import home, proy, ProyectoListar, ProyectoCrear
+from .views import crearProyecto, home, listarProyectos, proy
 
 urlpatterns = [
     path("", proy, name="proy"),
     path("home/", home, name="home"),
-    path("nuevo/", ProyectoCrear.as_view(), name="nuevo_proyecto"),
-    path("listar_proyectos/", ProyectoListar.as_view(), name="listar_proyectos"),
+    path("nuevo/", crearProyecto.as_view(), name="nuevo_proyecto"),
+    path("listar/", listarProyectos.as_view(), name="listar_proyectos"),
 ]
