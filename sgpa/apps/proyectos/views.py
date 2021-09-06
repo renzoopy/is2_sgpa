ec6af80220a16f73d73b5bf2fcb81ff16f7e4503
renzoopy <lorenzocabrera8@gmail.com>
2021-09-05 22:03:22 -0400

    Actualizaciones en las definiciones de las vistas

--- sgpa/apps/proyectos/views.py
+++ sgpa/apps/proyectos/views.py
@@ -1,11 +1,11 @@
 from usuarios.models import Perfil
+from miembros.models import Miembro
 from proyectos.models import Proyecto
 from django.urls.base import reverse_lazy
 from django.views.generic import ListView
-from django.contrib.auth.models import Group, User
 from django.shortcuts import redirect, render
-
 from proyectos.forms import ProyectoAdminForm
+from django.contrib.auth.models import Group, User
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
@@ -23,7 +23,7 @@ def home(request):
     return render(request, "home.html")
 
 
-class ProyectoCrear(LoginRequiredMixin, ListView):
+class crearProyecto(LoginRequiredMixin, ListView):
     redirect_field_name = "redirect_to"
     model = Proyecto
     form_class = ProyectoAdminForm
@@ -32,13 +32,14 @@ class ProyectoCrear(LoginRequiredMixin, ListView):
     def get_success_url(self):
         scrummaster = Perfil.objects.get(id=self.request.POST["srummaster"])
         proyecto = Proyecto.objects.get(id=self.object.pk)
-        equipo = Group.objects.create(name="comite%s" % self.object.pk)
+        Miembro.objects.create(idPerfil=scrummaster, idProyecto=proyecto)
+        equipo = Group.objects.create(name="equipo%s" % self.object.pk)
         proyecto.equipo = equipo
         proyecto.save()
-        return reverse_lazy("proyectos:sprint")
+        return reverse_lazy("usuarios:administrador")
 
 
-class ProyectoListar(LoginRequiredMixin, ListView):
+class listarProyectos(LoginRequiredMixin, ListView):
     redirect_field_name = "redirect_to"
     model = Proyecto
     template_name = "proyectos/listarProyectos.html"
