fa8d4db84d1c513d2cbd05824866a711d5b36b83
renzoopy <lorenzocabrera8@gmail.com>
2021-09-05 22:06:57 -0400

    Nuevos permisos agregados

--- sgpa/apps/usuarios/models.py
+++ sgpa/apps/usuarios/models.py
@@ -18,7 +18,7 @@ class Perfil(models.Model):
 
     class Meta:
         permissions = (
-            ("autorizar_usuario", "Permite autorizar el acceso a SGPA"),
+            ("autorizar_usuario", "Permite la autorizacion de acceso al SGPA"),
             ("acceso_usuario", "Permite el acceso a SGPA"),
             ("editar_usuario", "Permite editar usuario"),
             ("eliminar_usuario", "Permite eliminar usuario"),
