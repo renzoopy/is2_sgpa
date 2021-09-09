b19d5a26767d3313d9c2874f4c44890b8f33539e
renzoopy <lorenzocabrera8@gmail.com>
2021-09-08 21:09:24 -0400

    Ya es posible eliminar un proyecto

--- sgpa/apps/proyectos/urls.py
+++ sgpa/apps/proyectos/urls.py
@@ -2,11 +2,12 @@ from os import name
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import crearProyecto, home, listarProyectos, proy
+from .views import crearProyecto, home, listarProyectos, proy, eliminarProyecto
 
 urlpatterns = [
     path("", proy, name="proy"),
     path("home/", home, name="home"),
     path("nuevo/", crearProyecto.as_view(), name="nuevo_proyecto"),
     path("listar/", listarProyectos.as_view(), name="listar_proyectos"),
+    path("eliminar/<int:id_proyecto>/", eliminarProyecto, name="eliminar_proyectos"),
 ]
