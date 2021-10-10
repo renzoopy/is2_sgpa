e70b9900821059ab83ef613e0aeb060540751c7c
renzoopy <lorenzocabrera8@gmail.com>
2021-10-10 15:29:58 -0300

    Test agregados

--- sgpa/apps/tareas/tests.py
+++ sgpa/apps/tareas/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+import pytest
+import datetime
+from tareas.models import UserStory
+from usuarios.models import User, Perfil
+from proyectos.models import Proyecto, Sprint
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_crearItem():
+    # Verifica la creación de un item
+    usuario = User.objects.create_user("Won", "won@seo.com", "hyungwon")
+    perfil = Perfil.objects.create(ci=108108, telefono=108108, user=usuario)
+    proyecto = Proyecto.objects.create(
+        nombre="Bartender",
+        descripcion="Curso de Bartender",
+        fechaCreacion=datetime.date.today(),
+        numSprints=0,
+        estado="Pendiente",
+        scrumMaster=perfil,
+    )
+    sprint = Sprint.objects.create(numTareas=1, estado="Iniciado", proyecto=proyecto)
+
+    tarea = UserStory.objects.create(
+        nombre="Prueba",
+        descripcion="Descripcion",
+        fechaCreacion=datetime.date.today(),
+        estado="En_Cola",
+        desarrollador=perfil,
+    )
+
+    assert UserStory.objects.count() == 1
