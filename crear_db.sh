bc621f1eb4e6c37fee71fe97954b136bc28e2900
ricklegac <rick.legac@gmail.com>
2021-11-19 11:21:52 -0300

    crear_db.sh

---
+++ crear_db.sh
@@ -0,0 +1,11 @@
+sudo service postgresql start
+sudo su - postgres -c "createuser -s $USER"
+sudo -u postgres psql -c "ALTER USER $USER WITH SUPERUSER;"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $USER;"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $USER;"
+sudo -u postgres psql -c "CREATE DATABASE $USER;"
+sudo -u postgres -i psql --command "DROP ROLE IF EXISTS admin_proyecto; CREATE ROLE admin_proyecto superuser;";
+sudo -u postgres -i dropdb --if-exists sgpa; createdb sgpa;
+pipenv shell && \
+python3 manage.py migrate --run-syncdb
+
